42a29056d3b2e0ae2432d15d55782aa2d82f8876
Hemant Kumar Singh <hemantkumarsingh@Hemants-MacBook-Air.local>
2018-03-05 22:02:12 +0530

    get single object (author)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,11 +8,13 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  courses$: Observable<any[]>;;
+  courses$: Observable<any[]>;
+  author$: Observable<any>;
   constructor(private db: AngularFireDatabase) { }
 
   ngOnInit() {
     this.courses$ = this.db.list('/courses').valueChanges();
+    this.author$ = this.db.object('/authors/1').valueChanges();
   }
 
 }
