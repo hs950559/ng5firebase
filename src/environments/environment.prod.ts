d80475e30ea9756d560e09f55e1da20930ce1cc4
Hemant Kumar Singh <hemantkumarsingh@Hemants-MacBook-Air.local>
2018-03-06 15:03:05 +0530

    firebase hosting done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDDLH325cLVeHNshpt93znZqSdWCxo0d0M',
+    authDomain: 'ngapp-6e5f4.firebaseapp.com',
+    databaseURL: 'https://ngapp-6e5f4.firebaseio.com',
+    projectId: 'ngapp-6e5f4',
+    storageBucket: 'ngapp-6e5f4.appspot.com',
+    messagingSenderId: '713622077509'
+  }
 };
