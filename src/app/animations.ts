2447eda71ba3a9ec9eee51029c23c5f239e7b529
Hemant Kumar Singh <hemantkumarsingh@Hemants-MacBook-Air.local>
2018-03-07 12:07:12 +0530

    re-using animation - step 2

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,4 +1,21 @@
-import { transition, style, trigger, animate, state, keyframes } from '@angular/animations';
+import { transition, style, trigger, animate, state, keyframes, animation } from '@angular/animations';
+
+export let bounceOutLeft = animation(
+  animate('5s cubic-bezier(1,.25,.05,.99)', keyframes([
+    // 20%
+    style({
+      offset: .2,
+      opacity: 1,
+      transform: 'translate3d(120px, 0, 0)'
+    }),
+    // 100%
+    style({
+      offset: 1,
+      opacity: 0,
+      transform: 'translate3d(-100%, 0, 0)'
+    })
+  ]))
+);
 
 export let fade = trigger('fade', [
   state('void', style({opacity: 0 })),
@@ -12,20 +29,5 @@ export let slide = trigger('slide', [
     style({transform: 'translateX(-20px)'}),
     animate(300)
   ]),
-  transition(':leave', [
-    animate('5s cubic-bezier(1,.25,.05,.99)', keyframes([
-      // 20%
-      style({
-        offset: .2,
-        opacity: 1,
-        transform: 'translate3d(120px, 0, 0)'
-      }),
-      // 100%
-      style({
-        offset: 1,
-        opacity: 0,
-        transform: 'translate3d(-100%, 0, 0)'
-      })
-    ]))
-  ])
+  transition(':leave', bounceOutLeft)
 ]);
