ef8c325d23f1e033848ea5201ded034a832564aa
Hemant Kumar Singh <hemantkumarsingh@Hemants-MacBook-Air.local>
2018-03-07 15:48:50 +0530

    multi step animation

--- src/app/zippy/zippy.component.ts
+++ src/app/zippy/zippy.component.ts
@@ -1,9 +1,36 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { state, style, transition, trigger, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-zippy',
   templateUrl: './zippy.component.html',
-  styleUrls: ['./zippy.component.scss']
+  styleUrls: ['./zippy.component.scss'],
+  animations: [
+    trigger('expandCollapse', [
+      state('collapsed', style({
+        height: 0,
+        paddingTop: 0,
+        paddingBottom: 0,
+        overflow: 'hidden',
+        opacity: 0
+      })),
+
+      transition('collapsed => expanded', [
+        animate('500ms ease-out', style({
+          height: '*',
+          padding: '*',
+          overflow: 'auto'
+        })),
+        animate('1s', style({
+          opacity: 1
+        }))
+      ]),
+
+      transition('expanded => collapsed', [
+        animate('10s ease-in')
+      ])
+    ])
+  ]
 })
 export class ZippyComponent {
   @Input('title') title: string;
